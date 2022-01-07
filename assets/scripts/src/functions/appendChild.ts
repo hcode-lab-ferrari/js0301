30ef81b2ea18676e56819e471f164d69f9f8dbe9
João Rangel <joaohcrangel@gmail.com>
2022-01-06 22:14:59 -0300

    alterações

--- assets/scripts/src/functions/appendChild.ts
+++ assets/scripts/src/functions/appendChild.ts
@@ -1,11 +1,8 @@
 export default function appendChild(tagName: string, html: string, target: HTMLElement) {
 
     const el = document.createElement(tagName);
-
     el.innerHTML = html;
-
     target.appendChild(el);
-
     return el;
 
 }
